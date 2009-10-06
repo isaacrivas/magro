343067933fc31270b138252142b65c3c242ad7c0
null <null@Null.(none)>
2009-10-05 23:24:30 -0500

    Lint on env.py

--- magro/env.py
+++ magro/env.py
@@ -1,18 +1,32 @@
+"""This module contains the objects used for the
+configuration of the magro engine.
+
+path is a list of search paths where magro template libraries can be found.
+settings is a dictionary containing the string values that should be
+available in the context for all templates.
+
+"""
 import os
 
-PATHVARNAME='MAGRO_PATH'
-path=['.']
-settings={}
+__all__ = [ 'path', 'settings', 'searchfile' ]
+
+PATHVARNAME = 'MAGRO_PATH'
+path = ['.']
+settings = {}
 
 if os.environ.has_key(PATHVARNAME):
-    pathvar = os.environ[PATHVARNAME]
-    for p in pathvar.split( os.pathsep ):
-        path.append(p)
+    PATHVAR = os.environ[PATHVARNAME]
+    for path_entry in PATHVAR.split( os.pathsep ):
+        path.append( path_entry )
 
         
 def searchfile( filename ):
-    for p in path:
-        fullname = p + os.sep + filename
+    """
+    Find the fullpath of a file by searching in all registered
+    paths.
+    """
+    for _path in path:
+        fullname = _path + os.sep + filename
         if os.path.exists( fullname ):
             return os.path.abspath(fullname)
     return None
