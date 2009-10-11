bc2247e7bb5cce7f44db6c302a70a66382b62b32
null <null@Null.(none)>
2009-10-11 13:55:25 -0500

    lint on magro/django

--- magro/django/__init__.py
+++ magro/django/__init__.py
@@ -1,3 +1,13 @@
+"""
+This module can be used to enable magro templates through the
+django templates engine.
+
+This script will replace the django Template class with an
+implementation of it using magro.
+
+To make this available just import the magro.django package.
+
+"""
 from django.http import HttpResponse, Http404
 from django.conf import settings
 import magro.parser
@@ -7,23 +17,33 @@ from magro.context import Context
 import django.template as original
 
 class ResponseMiddleware:
+    """
+    Django Middleware for processing all responses as magro templates.
+    """
+    def __init__(self):
+        pass
+    
     def process_response(self, request, response):
+        "Processes all the responses marked by an X-MAGRO property"
         if 'X-MAGRO' in response:
             prepare_env()
             response.content =  magro.parser.parse( response.content )
         return response
 
 class FalseNodeList(list):
-    def __init__(self,target):
+    "Dummy node list used by this the Template class of this module."
+    def __init__(self, target):
         self.target = target
 
     def render(self, context):
+        "Evaluates the magro templates passed on construction."
         while hasattr( context, 'dicts' ):
             context = context.dicts[0]
         self.target.rootnode.eval(Context(context))
         
 class Template( object ):
-
+    """Replace the django Template class with this one. Can be used with
+    standard django templates or with magro files."""
     def __init__(self, template_string, origin=None, name='<Unknown Template>'):
         self.original = None
         if '{%' in template_string:
@@ -33,6 +53,7 @@ class Template( object ):
         self.name = name
 
     def get_nodelist(self):
+        "Creates a nodelist for django code that uses this function."
         if self.original:
             return self.original.nodelist
         else:
