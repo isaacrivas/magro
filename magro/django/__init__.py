ed09390a6d98204315fc519ce56ed7de46c00094
null <null@Null.(none)>
2009-11-01 21:59:44 -0600

    django package rewrite

--- magro/django/__init__.py
+++ magro/django/__init__.py
@@ -10,11 +10,10 @@ To make this available just import the magro.django package.
 """
 from django.http import HttpResponse, Http404
 from django.conf import settings
-import magro.parser
-import magro.env
+from magro import Environment, Template
 from magro.context import Context
 
-import django.template as original
+__all__ = ['ResponseMiddleware','render_to_response']
 
 class ResponseMiddleware:
     """
@@ -26,58 +25,25 @@ class ResponseMiddleware:
     def process_response(self, request, response):
         "Processes all the responses marked by an X-MAGRO property"
         if 'X-MAGRO' in response:
-            prepare_env()
-            response.content =  magro.parser.parse( response.content )
+            env = _init_magro_env()
+            template = Template( response.content, env )
+            response.content = template.render()
         return response
 
-class FalseNodeList(list):
-    "Dummy node list used by this the Template class of this module."
-    def __init__(self, target):
-        self.target = target
+def render_to_response( template_id, context=None ):
+    env = _init_magro_env()
+    Template = env.get_template( template_id )
+    
+    ctx = context
+    while hasattr( ctx, 'dicts' ):
+        ctx = ctx.dicts[0]
+    
+    return HttpResponse( Template.render( Context(ctx) ) )
 
-    def render(self, context):
-        "Evaluates the magro templates passed on construction."
-        while hasattr( context, 'dicts' ):
-            context = context.dicts[0]
-        self.target.rootnode.eval(Context(context))
-        
-class Template( object ):
-    """Replace the django Template class with this one. Can be used with
-    standard django templates or with magro files."""
-    def __init__(self, template_string, origin=None, name='<Unknown Template>'):
-        self.original = None
-        if '{%' in template_string:
-            self.original = OriginalTemplateClass( template_string, origin, name )
-        else:
-            self.rootnode = magro.parser.compile( template_string )
-        self.name = name
-
-    def get_nodelist(self):
-        "Creates a nodelist for django code that uses this function."
-        if self.original:
-            return self.original.nodelist
-        else:
-            return FalseNodeList( self )
-        
-    nodelist = property(get_nodelist)
-        
-    def __iter__(self):
-        if self.original:
-            yield self.original.__iter__()
-        else:
-            for node in self.rootnode.code:
-                for subnode in node.code:
-                    yield subnode
-
-    def render(self, context):
-        "Display stage -- can be called many times"
-        if self.original:
-            return self.original.render(context)
-        else:
-            while hasattr( context, 'dicts' ):
-                context = context.dicts[0]
-            return self.rootnode.eval(Context(context))
-
-
-OriginalTemplateClass = original.Template
-original.Template = Template
+def _init_magro_env():
+    globs = globals()
+    if '_MAGRO_ENV' not in globs:
+        _MAGRO_ENV = Environment()
+        _MAGRO_ENV.loader.path.extend( settings.TEMPLATE_DIRS )
+        globs['_MAGRO_ENV'] = _MAGRO_ENV
+    return globs['_MAGRO_ENV']
